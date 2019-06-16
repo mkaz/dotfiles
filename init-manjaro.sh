3909e86c7a5a4168129cd8d97dc8ba61640ad713
Marcus Kazmierczak <marcus@mkaz.com>
2019-06-16 09:25:04 -0700

    Add muparser for albert calc support

--- init-manjaro.sh
+++ init-manjaro.sh
@@ -21,7 +21,7 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm binutils make
 sudo pacman -S --noconfirm stow ctags pwgen net-tools dnsutils htop autossh 
 sudo pacman -S --noconfirm pass autojump task neofetch ripgrep figlet
-sudo pacman -S --noconfirm pngcrush pandoc peek gpick cowsay albert
+sudo pacman -S --noconfirm pngcrush pandoc peek gpick cowsay albert muparser
 
 ## Setup configs
 cd $HOME/dotfiles/
