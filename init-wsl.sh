27aa8d1d077745f916cd81e3c1f1a9f03c6bcfb9
Marcus Kazmierczak <marcus@mkaz.com>
2020-02-22 12:26:14 -0800

    Init for wsl

---
+++ init-wsl.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Install script for setting up my Manjaro linux
+
+UNAME=`uname`
+cd $HOME
+
+rm -f .profile
+ln -s dotfiles/profile .profile
+
+mkdir ~/bin/
+mkdir ~/src/
+mkdir ~/tmp/
+
+## update & upgrade
+sudo apt update && apt upgrade
+
+## use stow to configure rcfiles
+cd ~/dotfiles/
+
+sudo apt install -y git sudo zip curl wget vim-nox
+sudo apt install -y build-essential stow pwgen htop autojump neofetch
+sudo apt install -y net-tools dnsutils autossh apt-transport-https
+sudo apt install -y ripgrep fzf newsboat figlet 
+
+# connect configs
+stow rcfiles
+
+# Golang
+sudo apt install -y golang
+
+# LAMP
+read -p "Install LAMP Stack? (Y/n) " lamp
+if [ "$lamp" != "${lamp#[Nn]}" ]; then
+	echo "✖ Skipping LAMP"
+else
+	echo "✔ Installing Sublime Text"
+    sudo apt install -y apache2
+    sudo apt install -y mariadb-client mariadb-server
+    sudo apt install -y php libapache2-mod-php php-cli php-curl php-gd php-imagick php-intl php-mbstring php-mysql php-xml
+    sudo a2enmod rewrite expires vhost_alias ssl headers
+fi
+
+# wp cli
+if [ ! -f "$HOME/bin/wp" ]; then
+    cd $HOME/Downloads
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    cp wp-cli.phar $HOME/bin/wp
+    cd
+fi
+
