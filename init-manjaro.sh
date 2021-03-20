9ff074e3eb3bac3ccebf5d741b7156f002bf87eb
Marcus Kazmierczak <marcus@mkaz.com>
2021-03-19 21:46:37 -0700

    Add julia and sublime to install

--- init-manjaro.sh
+++ init-manjaro.sh
@@ -15,7 +15,7 @@ mkdir ~/Downloads/
 
 ## update & upgrade
 sudo pacman -Syu --needed --noconfirm base-devel stow pwgen htop pass neofetch autossh
-sudo pacman -Syu --needed --noconfirm bat exa fd flameshot fzf jq ripgrep yay
+sudo pacman -Syu --needed --noconfirm bat exa fd flameshot fzf jq ripgrep yay julia
 sudo pacman -Syu --needed --noconfirm powerline-fonts ttf-roboto
 
 # gnome extensions
@@ -81,10 +81,9 @@ if [ "$sublime" != "${sublime#[Nn]}" ]; then
 	echo "✖ Skipping Sublime Text"
 else
 	echo "✔ Installing Sublime Text"
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	apt update
-	apt install -y sublime-text
+	curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+	echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+	sudo pacman -Syu --needed --noconfirm sublime-text
 fi
 
 cd $HOME/Downloads
