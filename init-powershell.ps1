f418a9021d7ab74798b7cb10aca02882b7f54807
Marcus Kazmierczak <marcus@mkaz.com>
2021-10-09 06:56:49 -0700

    Rearrange rcfiles and extras into proper configs

---
+++ init-powershell.ps1
@@ -0,0 +1,16 @@
+# Windows Powershell setup
+
+scoop bucket add extras
+
+# basics
+scoop install 7zip gh git neovim
+
+# ruby utils
+scoop install bat espanso fd ripgrep starship
+
+# Apps from extras
+scoop install audacity hack-font obs-studio
+scoop install languagetool obsidian sublime-text
+scoop install signal slack syncthingtray
+
+# Copy configs to correct location
