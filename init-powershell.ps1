84c2a8900f56ca056911e0370379e7650bf63c21
Marcus Kazmierczak <marcus@mkaz.com>
2021-10-09 07:10:56 -0700

    Clean up configs, copy to right spots

--- init-powershell.ps1
+++ init-powershell.ps1
@@ -14,3 +14,11 @@ scoop install languagetool obsidian sublime-text
 scoop install signal slack syncthingtray
 
 # Copy configs to correct location
+# TODO: symlink or sync
+Copy-Item "configs\nvim" -Destination "$HOME\AppData\Local\nvim" -Recurse
+
+Copy-Item "configs\espanso\default.yml" -Destination "$HOME\scoop\apps\espanso\current\.espanso\default.yml"
+
+Copy-Item "configs\git\gitattributes" -Destination "$HOME\.gitattributes"
+Copy-Item "configs\git\gitconfig" -Destination "$HOME\.gitconfig"
+Copy-Item "configs\git\gitignore" -Destination "$HOME\.gitignore"
