22e0bf50e6a2edefcc45dc1fceb844b4b09421a0
Marcus Kazmierczak <marcus@mkaz.com>
2021-10-29 13:54:53 -0700

    Add Volta, Starship and recent updates

--- init-wsl.sh
+++ init-wsl.sh
@@ -16,30 +16,30 @@ mkdir ~/.config/
 ## update & upgrade
 sudo apt update && sudo apt upgrade
 
-## use stow to configure rcfiles
-cd ~/dotfiles/
-
 sudo apt install -y git sudo zip curl wget vim-nox
-sudo apt install -y build-essential stow pwgen htop autojump neofetch
+sudo apt install -y build-essential pwgen htop autojump neofetch
 sudo apt install -y net-tools dnsutils autossh apt-transport-https
-sudo apt install -y ripgrep fzf newsboat figlet pass jq
+sudo apt install -y ripgrep fzf figlet pass jq
 
-# connect configs
-stow rcfiles
+# link up configs
+ln -s $HOME/dotfiles/configs/git/gitattributes $HOME/.gitattributes
+ln -s $HOME/dotfiles/configs/git/gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/configs/git/gitignore $HOME/.gitignore
 
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 
-# Node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-## need to init
-#nvm install --lts
+# Starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+# Volta
+curl https://get.volta.sh | bash
 
 # Neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage $HOME/bin/nvim
-ln -s $HOME/dotfiles/extras/nvim $HOME/.config/
+ln -s $HOME/dotfiles/configs/nvim $HOME/.config/
 
 # LAMP
 read -p "Install LAMP Stack? (Y/n) " lamp
@@ -55,7 +55,7 @@ fi
 
 # wp cli
 if [ ! -f "$HOME/bin/wp" ]; then
-    cd $HOME/Downloads
+    cd $HOME/tmp/
     wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     cp wp-cli.phar $HOME/bin/wp
