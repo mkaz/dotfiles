6b6ce3baa0a790528027baed537e24b1157925c0
Marcus Kazmierczak <marcus@mkaz.com>
2022-04-07 06:42:05 -0700

    Minor tweaks for pi

--- init-pi.sh
+++ init-pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install script for setting up my Manjaro linux
+# Install script for setting up a Raspberry Pi
 
 UNAME=`uname`
 cd $HOME
@@ -16,10 +16,10 @@ mkdir ~/.config/
 ## update & upgrade
 sudo apt update && sudo apt upgrade
 
-sudo apt install -y git sudo zip curl wget vim-nox
+sudo apt install -y git sudo zip curl wget neovim
 sudo apt install -y build-essential pwgen htop autojump neofetch
 sudo apt install -y net-tools dnsutils autossh apt-transport-https
-sudo apt install -y ripgrep fzf figlet pass jq
+sudo apt install -y ripgrep fzf figlet jq exa
 
 # link up configs
 ln -s $HOME/dotfiles/configs/git/gitattributes $HOME/.gitattributes
@@ -29,24 +29,12 @@ ln -s $HOME/dotfiles/configs/git/gitignore $HOME/.gitignore
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 
-# Starship
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-
-# Volta
-curl https://get.volta.sh | bash
-
-# Neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-mv nvim.appimage $HOME/bin/nvim
-ln -s $HOME/dotfiles/configs/nvim $HOME/.config/
-
 # LAMP
 read -p "Install LAMP Stack? (Y/n) " lamp
 if [ "$lamp" != "${lamp#[Nn]}" ]; then
 	echo "✖ Skipping LAMP"
 else
-	echo "✔ Installing Sublime Text"
+	echo "✔ Installing LAMP Stack"
     sudo apt install -y apache2
     sudo apt install -y mariadb-client mariadb-server
     sudo apt install -y php libapache2-mod-php php-cli php-curl php-gd php-imagick php-intl php-mbstring php-mysql php-xml
