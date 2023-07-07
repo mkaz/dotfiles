3bad09f002fa05ae5231cbb9cafe324ff1f90f22
Marcus Kazmierczak <marcus@mkaz.com>
2023-07-07 09:09:02 -0700

    Install updates for Debian

--- init-deb.sh
+++ init-deb.sh
@@ -20,11 +20,12 @@ sudo apt update && sudo apt upgrade -y
 ## use stow to configure rcfiles
 cd ~/dotfiles/
 
-sudo apt install -y git git-lfs sudo zip curl wget ufw neovim neovim-qt
-sudo apt install -y build-essential stow pwgen htop pass autojump neofetch
+sudo apt install -y git zip curl wget ufw neovim neovim-qt
+sudo apt install -y build-essential pwgen htop pass autojump neofetch uuid-runtime
 sudo apt install -y net-tools dnsutils autossh apt-transport-https syncthing
 sudo apt install -y gnome-tweaks gnome-shell-pomodoro gnome-dictionary
-sudo apt install -y bat exa fd fzf jq ripgrep 
+sudo apt install -y bat exa fzf fzy ripgrep
+sudo apt install -y fonts-hack fonts-ibm-plex fonts-cabin fonts-roboto fonts-noto-color-emoji
 
 # link up configs
 ln -s $HOME/dotfiles/configs/git/gitattributes $HOME/.gitattributes
@@ -35,48 +36,10 @@ ln -s $HOME/dotfiles/configs/git/gitignore $HOME/.gitignore
 mkdir -p $HOME/.config
 ln -s $HOME/dotfiles/configs/nvim $HOME/.config/nvim
 
-# gui: photo and video
-read -p "Install Photo & Video? (N/y) " media
-if [ "$media" != "${media#[Yy]}" ]; then
-    sudo apt install -y imagemagick pngcrush pandoc gifsicle flameshot
-    sudo apt install -y rapid-photo-downloader geeqie peek gpick 
-fi
-
-# gui: fonts
-read -p "Install Fonts? (N/y) " fonts
-if [ "$fonts" != "${fonts#[Yy]}" ]; then
-	sudo apt install -y fonts-hack fonts-ibm-plex fonts-noto fonts-roboto fonts-noto-color-emoji
-fi
-
-#----------------------------------------
-
-# Install Node using volta
-curl https://get.volta.sh | bash
 
 # enable and start syncthing
 sudo systemctl enable syncthing@mkaz.service
 sudo systemctl start syncthing@mkaz.service
-
-# LAMP (use Local?)
-read -p "Install LAMP Stack? (Y/n) " lamp
-if [ "$lamp" != "${lamp#[Nn]}" ]; then
-	echo "✖ Skipping LAMP"
-else
-	echo "✔ Installing LAMP"
-    sudo apt install -y apache2
-    sudo apt install -y mariadb-client mariadb-server
-    sudo apt install -y php libapache2-mod-php php-cli php-curl php-gd php-imagick php-intl php-mbstring php-mysql php-xml
-    sudo a2enmod rewrite expires vhost_alias ssl headers
-fi
-
-# wp cli
-if [ ! -f "$HOME/bin/wp" ]; then
-    cd $HOME/Downloads
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
-    cp wp-cli.phar $HOME/bin/wp
-    cd
-fi
 
 # configure firewall
 sudo ufw allow ssh
@@ -85,26 +48,11 @@ sudo ufw allow 22000        # syncthing
 sudo ufw allow 21027/udp    # syncthing
 sudo ufw enable
 
-# disable printer auto discovery
-# easier to configure and setup manually
-sudo systemctl stop cups-browsed
-sudo systemctl disable cups-browsed
-
-# Software installs
-
-
-cd $HOME/Downloads
-
-# Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
-
-## install exa
-wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
-unzip exa-linux-x86_64-0.9.0.zip
-mv exa-linux-x86_64 ~/bin/exa
+# Python
+sudo apt install -y python3-pip
 
 # espanso
-wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-amd64.deb
-sudo apt install -y ./espanso-debian-amd64.deb
+# wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-amd64.deb
+# sudo apt install -y ./espanso-debian-amd64.deb
 
 
